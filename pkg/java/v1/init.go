f41261f4ce0786a5306fb97e445614caf59f7b98
jesus m. rodriguez <jesusr@redhat.com>
2021-03-18 00:01:47 -0400

    Make init implement RunOptions

--- pkg/java/v1/init.go
+++ pkg/java/v1/init.go
@@ -19,6 +19,8 @@ package v1
 import (
 	"fmt"
 
+	"github.com/java-operator-sdk/kubebuilder-plugin/pkg/internal/kubebuilder/cmdutil"
+	"github.com/java-operator-sdk/kubebuilder-plugin/pkg/java/v1/scaffolds"
 	"github.com/spf13/pflag"
 
 	"sigs.k8s.io/kubebuilder/v3/pkg/config"
@@ -28,12 +30,18 @@ import (
 
 type initSubcommand struct {
 	config config.Config
+
 	// For help text.
 	commandName string
+
+	// Flags
+	domain      string
+	projectName string
 }
 
 var (
 	_ plugin.InitSubcommand = &initSubcommand{}
+	_ cmdutil.RunOptions    = &initSubcommand{}
 )
 
 func (p *initSubcommand) UpdateContext(ctx *plugin.Context) {
@@ -47,6 +55,8 @@ Writes the following files:
 }
 
 func (p *initSubcommand) BindFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&p.domain, "domain", "my.domain", "domain for groups")
+	fs.StringVar(&p.projectName, "project-name", "", "name of this project")
 }
 
 func (p *initSubcommand) InjectConfig(c config.Config) {
@@ -55,5 +65,30 @@ func (p *initSubcommand) InjectConfig(c config.Config) {
 
 func (p *initSubcommand) Run(fs machinery.Filesystem) error {
 	fmt.Println("init called")
+
+	if err := p.config.SetProjectName(p.projectName); err != nil {
+		return err
+	}
+
+	if err := p.config.SetDomain(p.domain); err != nil {
+		return err
+	}
+
+	if err := cmdutil.Run(p, fs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *initSubcommand) Validate() error {
+	return nil
+}
+
+func (p *initSubcommand) GetScaffolder() (cmdutil.Scaffolder, error) {
+	return scaffolds.NewInitScaffolder(p.config), nil
+}
+
+func (p *initSubcommand) PostScaffold() error {
 	return nil
 }
