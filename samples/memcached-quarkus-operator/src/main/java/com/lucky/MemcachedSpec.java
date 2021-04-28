3fd095e5e98b6db1f807767e3462113b888b6047
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2021-04-28 00:43:16 -0700

    added sample for memcached

---
+++ samples/memcached-quarkus-operator/src/main/java/com/lucky/MemcachedSpec.java
@@ -0,0 +1,16 @@
+package com.lucky;
+
+public class MemcachedSpec {
+
+    // Add Spec information here
+
+    private Integer size;
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
