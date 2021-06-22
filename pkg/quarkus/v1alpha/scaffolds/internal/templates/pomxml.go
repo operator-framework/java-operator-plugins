cba1e75f5f32f60317188b061e9a2ae558afdb02
Chris Laprun <metacosm@gmail.com>
2021-06-22 18:30:35 +0200

    fix: no need to specify a builder image

--- pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
+++ pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
@@ -60,7 +60,6 @@ const pomxmlTemplate = `<?xml version="1.0" encoding="UTF-8"?>
     <fabric8-client.version>5.4.1</fabric8-client.version>
     <quarkus-sdk.version>1.9.4</quarkus-sdk.version>
     <quarkus.version>1.13.7.Final</quarkus.version>
-    <quarkus.native.builder-image>quay.io/quarkus/ubi-quarkus-native-image:19.3.1-java11</quarkus.native.builder-image>
   </properties>
 
   <dependencyManagement>
