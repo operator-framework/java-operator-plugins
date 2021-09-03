db6faeb9ecc7b13bedf59f850b243cd423abf3a1
Chris Laprun <metacosm@gmail.com>
2021-09-03 18:27:30 +0200

    fix: no need to add Kind and Plural with default versions

--- pkg/quarkus/v1alpha/scaffolds/internal/templates/model/model.go
+++ pkg/quarkus/v1alpha/scaffolds/internal/templates/model/model.go
@@ -55,14 +55,10 @@ const modelTemplate = `package {{ .Package }};
 {{if .Resource.API.Namespaced}}import io.fabric8.kubernetes.api.model.Namespaced;{{end}}
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
-import io.fabric8.kubernetes.model.annotation.Kind;
-import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Version("{{ .Resource.Version }}")
 @Group("{{ .Resource.QualifiedGroup }}")
-@Kind("{{ .Resource.Kind }}")
-@Plural("{{ .Resource.Plural }}")
 public class {{ .ClassName }} extends CustomResource<{{ .ClassName }}Spec, {{ .ClassName }}Status> {{if .Resource.API.Namespaced}}implements Namespaced {{end}}{}
 
 `
