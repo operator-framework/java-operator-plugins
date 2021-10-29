5d714267d3b8af7851cc411a8c4318490e014b61
Chris Laprun <metacosm@gmail.com>
2021-10-29 19:19:33 +0200

    chore(deps): update to quarkus 2.4.0, fabric8 5.8.0 and extension 2.0.0

--- pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
+++ pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
@@ -57,9 +57,9 @@ const pomxmlTemplate = `<?xml version="1.0" encoding="UTF-8"?>
     <maven.compiler.target>11</maven.compiler.target>
     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
     <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-    <fabric8-client.version>5.7.2</fabric8-client.version>
-    <quarkus-sdk.version>2.0.0.Beta5</quarkus-sdk.version>
-    <quarkus.version>2.2.1.Final</quarkus.version>
+    <fabric8-client.version>5.8.0</fabric8-client.version>
+    <quarkus-sdk.version>2.0.0</quarkus-sdk.version>
+    <quarkus.version>2.4.0.Final</quarkus.version>
   </properties>
 
   <dependencyManagement>
