70125e8bfc4849f8d18c8b7d69032fdfbd5c1a8a
metacosm <metacosm@users.noreply.github.com>
2023-09-12 13:15:03 +0000

    feat: update QOSDK to 6.3.1
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
+++ pkg/quarkus/v1alpha/scaffolds/internal/templates/pomxml.go
@@ -57,7 +57,7 @@ const pomxmlTemplate = `<?xml version="1.0" encoding="UTF-8"?>
     <maven.compiler.target>17</maven.compiler.target>
     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
     <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-    <quarkus-sdk.version>6.3.0</quarkus-sdk.version>
+    <quarkus-sdk.version>6.3.1</quarkus-sdk.version>
     <quarkus.version>3.2.4.Final</quarkus.version>
   </properties>
 
