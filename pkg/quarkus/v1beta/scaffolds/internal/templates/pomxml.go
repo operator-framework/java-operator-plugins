ec31558fb618f91a9ef1a87ff0d11218ff5af162
metacosm <metacosm@users.noreply.github.com>
2024-03-19 22:19:03 +0000

    feat: update QOSDK to 6.6.5

--- pkg/quarkus/v1beta/scaffolds/internal/templates/pomxml.go
+++ pkg/quarkus/v1beta/scaffolds/internal/templates/pomxml.go
@@ -57,8 +57,8 @@ const pomxmlTemplate = `<?xml version="1.0" encoding="UTF-8"?>
     <maven.compiler.target>17</maven.compiler.target>
     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
     <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-    <quarkus-sdk.version>6.6.4</quarkus-sdk.version>
-    <quarkus.version>3.8.2</quarkus.version>
+    <quarkus-sdk.version>6.6.5</quarkus-sdk.version>
+    <quarkus.version>3.8.3</quarkus.version>
   </properties>
 
   <dependencyManagement>
